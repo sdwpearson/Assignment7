831e4655c6f64a473a613f7a3ef1ad5de1713ea0
sdwpearson <sdwpearson@gmail.com>
2019-03-10 18:37:05 -0400

    [PHY1610] Initial code to simulate the random walkers

--- walkring_timestep.cc
+++ walkring_timestep.cc
@@ -32,11 +32,47 @@
 //
 void walkring_timestep(rarray<int,1>& walkerpositions, int N, double prob)
 {
-    ////////////////////////////////////////////////
-    //                                            //
-    // IMPLEMENT THIS AS PART OF YOUR ASSIGNMENT! //
-    //                                            //
-    ////////////////////////////////////////////////
+	int seed = 4;
+	double rand_num = 0.0;
+
+	// Copy the original positions
+	rarray<int,1> walkerpositions_old = walkerpositions.copy();
+	int Z_old = walkerpositions_old.size();
+
+	// Blank out the new positions
+	walkerpositions.fill(0);
+
+    static std::mt19937 engine(seed);
+    std::uniform_int_distribution<int> uniformint(0, total);
+
+    // Cycle through all of the positions
+    for (int i=0; i<N; i++){
+
+        // Go through each of the walkers at each position
+        for(int k=0; k<walkerpositions_old[i]; k++){
+			rand_num = uniformint(engine);
+
+        	if(rand_num >= 0.0 && rand_num < prob){ 			// Move Left
+        		if(k == 0)											// Loop around to the other side
+        			walkerpositions[N-1]++;
+        		else
+        			walkerpositions[k-1]++;
+        	}
+        	else if (rand_num >= prob && rand_num < 2*prob){	// Move Right
+        		if(k == N-1)											// Loop around to the other side
+        			walkerpositions[0]++;
+        		else
+        			walkerpositions[k+1]++;
+        	}
+        	else {												// Remain in current position
+    			walkerpositions[k]++;
+        	}
+    	}
+    }
+
+    Z_new = walkerpositions.size();
+    // Make sure the total amount of walkers remains unchanged
+    std::cout << Z_old << ' ' << Z_new << std::endl;
 }
 
 
