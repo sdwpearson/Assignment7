c136fe29e7e60aec82fa39d032c2cde75b685c07
sdwpearson <sdwpearson@gmail.com>
2019-03-10 20:31:13 -0400

    [PHY1610] Altered params.ini to have 90k walkers

--- diffring_timestep.cc
+++ diffring_timestep.cc
@@ -32,12 +32,12 @@ void diffring_fill_timestep_matrix(rarray<double,2>& F, double D, double dt, dou
 
     // Row index
     for(int i=0; i<N; i++){
-        if(i == 0){                 // B.C. #1
+        if(i == 0){                 // B.C. #1 Connect left side of the loop
             F[i][N-1] = alpha;
             F[i][1] = alpha;
             F[i][0] = 1-2*alpha;
         }
-        else if (i == (N-1)){       // B.C. #2
+        else if (i == (N-1)){       // B.C. #2 Connect Right side of the loop
             F[i][0] = alpha;
             F[i][N-2] = alpha;
             F[i][N-1] = 1-2*alpha;
